e41e144f93b39c1d136fd99498382d5e6cc04123
Juan Manuel Suárez García <jmsuarez@addi.com>
2022-07-09 12:58:34 -0500

    fix(config): Update website to be personal

--- src/App/config.tsx
+++ src/App/config.tsx
@@ -4,37 +4,37 @@ import { Email, GitHub, LinkedIn, Resume } from 'icons';
 
 const config: Config = {
   name: {
-    display: 'Adam Alston',
-    aria: 'My name is Adam Alston',
+    display: 'Juan Manuel Suárez',
+    aria: 'My name is Juan Manuel Suárez',
   },
   title: {
-    display: 'Software Engineer',
-    aria: 'I am a software engineer',
+    display: 'Analytics Engineer',
+    aria: 'I am an analytics engineer',
   },
   buttons: [
     {
       display: 'GitHub',
       aria: 'Visit my GitHub profile',
       icon: <GitHub />,
-      href: 'https://github.com/adamalston/',
+      href: 'https://github.com/jsuarezgg',
     },
     {
       display: 'LinkedIn',
       aria: 'Visit my LinkedIn profile',
       icon: <LinkedIn />,
-      href: 'https://www.linkedin.com/in/adam-alston/',
+      href: 'https://www.linkedin.com/in/jsuarezg/',
     },
     {
       display: 'Resume',
       aria: 'View my resume in Google Drive',
       icon: <Resume />,
-      href: 'https://drive.google.com/drive/folders/10k8NWflSYQ5laPzuWtK3bzUKzuOeas8i/',
+      href: 'https://jmsgpersonal.s3.amazonaws.com/Resume_Juan_Suarez.pdf',
     },
     {
       display: 'Email',
       aria: 'Send me an email',
       icon: <Email />,
-      href: 'mailto:aalston9@gmail.com',
+      href: 'mailto:jmsg99@hotmail.com',
     },
   ],
 };
